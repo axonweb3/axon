05d5612a6dd8f37a75fa821e6c2a6cb3ce01c3f4
Mohanson <mohanson@outlook.com>
2021-12-08 17:41:56 +0800

    feat(protocol): add executor traits (#7)

--- protocol/src/traits/executor.rs
+++ protocol/src/traits/executor.rs
@@ -1,1 +1,20 @@
+use crate::types::{Hash, SignedTransaction};
+use async_trait::async_trait;
+use evm::executor::stack::Log;
+use evm::ExitReason;
 
+pub struct ExecuteResult {
+    exit_reason: ExitReason,
+    ret:         Vec<u8>,
+    logs:        Vec<Log>,
+}
+
+pub struct BatchExecuteResult {
+    root:    Hash,
+    results: Vec<ExecuteResult>,
+}
+
+#[async_trait]
+pub trait Executor: Send + Sync {
+    async fn batch_execute(&self, signed_txs: Vec<SignedTransaction>) -> BatchExecuteResult;
+}
