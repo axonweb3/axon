c26c8d57363583d807ee220cbfb032995f27b945
KaoImin <kaoimin@qq.com>
2022-01-12 16:48:20 +0800

    cargo fmt

--- core/executor/src/debugger/uniswap2.rs
+++ core/executor/src/debugger/uniswap2.rs
@@ -3,8 +3,8 @@ use super::*;
 #[test]
 fn test_uniswap2() {
     let mut _debugger = EvmDebugger::new();
-	// let tx = Transaction {};
-	// let stx = mock_signed_tx(tx);
-	// let resp = debugger.exec(1, vec![stx]);
+    // let tx = Transaction {};
+    // let stx = mock_signed_tx(tx);
+    // let resp = debugger.exec(1, vec![stx]);
     clear_data();
 }
