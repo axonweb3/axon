454a8d76caa98cdd5d3fcdebfbd38a1d2ad50aee
KaoImin <kaoimin@qq.com>
2022-01-26 14:48:42 +0800

    fix(genesis): change the timestamp of genesis

--- core/consensus/src/engine.rs
+++ core/consensus/src/engine.rs
@@ -69,7 +69,12 @@ impl<Adapter: ConsensusAdapter + 'static> Engine<Proposal> for ConsensusEngine<A
         let status = self.status.inner();
         let txs = self
             .adapter
-            .get_txs_from_mempool(ctx.clone(), next_number, status.gas_limit, 10000)
+            .get_txs_from_mempool(
+                ctx.clone(),
+                next_number,
+                status.gas_limit,
+                METADATA_CONTROLER.load().current().tx_num_limit,
+            )
             .await?;
         let signed_txs = self.adapter.get_full_txs(ctx.clone(), &txs).await?;
         let order_root = Merkle::from_hashes(txs.clone())
@@ -608,12 +613,13 @@ impl<Adapter: ConsensusAdapter + 'static> ConsensusEngine<Adapter> {
             .save_signed_txs(ctx.clone(), block_number, txs)
             .await?;
 
-        // Save the block.
-        self.adapter.save_block(ctx.clone(), block.clone()).await?;
-
+        // Save transaction receipts
         self.adapter
             .save_receipts(ctx.clone(), block_number, receipts)
             .await?;
+
+        // Save the block
+        self.adapter.save_block(ctx.clone(), block.clone()).await?;
 
         let metadata = METADATA_CONTROLER.load().current();
         let new_status = CurrentStatus {
