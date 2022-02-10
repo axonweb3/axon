7970b009df1dcaef9274416fc7c52c715d5732e3
“zhengjianhui” <815168733@qq.com>
2022-02-10 23:13:59 +0800

    feat(apm): prom support

---
+++ common/apm/src/prom_server.rs
@@ -0,0 +1,20 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+
+pub fn prom_server() -> Router {
+    let app = axum::Router::new().route("/metrics", axum::routing::get(get_metrics));
+    app
+}
+
+async fn get_metrics() -> String {
+    let metrics_data = match super::metrics::all_metrics() {
+        Ok(data) => data,
+        Err(e) => e.to_string().into_bytes(),
+    };
+
+    String::from_utf8(metrics_data).unwrap()
+}
