1e1f64e0665fd1b0fc346a4c6674a3e4fd564e7c
KaoImin <kaoimin@qq.com>
2022-02-13 16:43:58 +0800

    refactor: metric_rpc adopt async_trait

---
+++ common/metrics-derive/src/pin_box_fut.rs
@@ -0,0 +1,111 @@
+use quote::quote;
+use syn::{
+    GenericArgument, PathArguments, ReturnType, TraitBound, Type, TypeParamBound, TypePath,
+    TypeTraitObject,
+};
+
+const PIN: &str = "Pin";
+const BOX: &str = "Box";
+const FUTURE: &str = "Future";
+const RESULT: &str = "Result";
+
+pub struct PinBoxFutRet {
+    pub is_pin_box_fut:    bool,
+    pub is_fut_ret_result: bool,
+    pub ret_ty:            proc_macro2::TokenStream,
+}
+
+impl Default for PinBoxFutRet {
+    fn default() -> Self {
+        PinBoxFutRet {
+            is_pin_box_fut:    false,
+            is_fut_ret_result: false,
+            ret_ty:            quote! {},
+        }
+    }
+}
+
+impl PinBoxFutRet {
+    pub fn parse(ret_ty: &ReturnType) -> PinBoxFutRet {
+        let expect_ty = match ret_ty {
+            ReturnType::Type(_, ty) => ty,
+            _ => return PinBoxFutRet::default(),
+        };
+
+        let expect_pin = match *(expect_ty.clone()) {
+            Type::Path(TypePath { qself: _, path }) => {
+                let last_seg = path.segments.last().cloned();
+                match last_seg.map(|ls| (ls.ident.clone(), ls)) {
+                    Some((ls_ident, ls)) if ls_ident.to_string() == PIN => ls,
+                    _ => return PinBoxFutRet::default(),
+                }
+            }
+            _ => return PinBoxFutRet::default(),
+        };
+
+        let expect_box = match &expect_pin.arguments {
+            PathArguments::AngleBracketed(wrapper) => match wrapper.args.last() {
+                Some(GenericArgument::Type(Type::Path(TypePath { qself: _, path }))) => {
+                    match path.segments.last().map(|ls| (ls.ident.clone(), ls)) {
+                        Some((ls_ident, ls)) if ls_ident == BOX => ls,
+                        _ => return PinBoxFutRet::default(),
+                    }
+                }
+                _ => return PinBoxFutRet::default(),
+            },
+            _ => return PinBoxFutRet::default(),
+        };
+
+        // Has Future trait bound
+        match &expect_box.arguments {
+            PathArguments::AngleBracketed(wrapper) => match wrapper.args.last() {
+                Some(GenericArgument::Type(Type::TraitObject(TypeTraitObject {
+                    dyn_token: _,
+                    bounds,
+                }))) => {
+                    let mut fut_ret = PinBoxFutRet::default();
+
+                    for bound in bounds.iter() {
+                        if let TypeParamBound::Trait(TraitBound { path, .. }) = bound {
+                            if let Some(arg) = path.segments.last() {
+                                if arg.ident == FUTURE {
+                                    fut_ret.is_pin_box_fut = true;
+                                    fut_ret.is_fut_ret_result =
+                                        is_fut_ret_result(&arg.arguments, &mut fut_ret);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    fut_ret
+                }
+                _ => PinBoxFutRet::default(),
+            },
+            _ => PinBoxFutRet::default(),
+        }
+    }
+}
+
+fn is_fut_ret_result(input: &PathArguments, fut_ret: &mut PinBoxFutRet) -> bool {
+    match input {
+        PathArguments::AngleBracketed(angle_arg) => {
+            match angle_arg.args.first().expect("future output") {
+                GenericArgument::Binding(binding) => match &binding.ty {
+                    Type::Path(path) => {
+                        fut_ret.ret_ty = quote! { #path };
+                        path.path
+                            .segments
+                            .last()
+                            .unwrap()
+                            .ident
+                            .to_string()
+                            .contains(RESULT)
+                    }
+                    _ => false,
+                },
+                _ => false,
+            }
+        }
+        _ => false,
+    }
+}
--- common/metrics-derive/src/rpc_expand.rs
+++ common/metrics-derive/src/rpc_expand.rs
@@ -1,38 +1,81 @@
 use proc_macro::TokenStream;
 use proc_macro2 as pm2;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, Ident, ItemFn, Lit, NestedMeta};
+use syn::{parse_macro_input, AttributeArgs, Ident, ItemFn, Lit, NestedMeta, ReturnType};
+
+use crate::pin_box_fut::PinBoxFutRet;
 
 pub fn expand_rpc_metrics(attr: TokenStream, func: TokenStream) -> TokenStream {
     let attr = parse_macro_input!(attr as AttributeArgs);
     let func = parse_macro_input!(func as ItemFn);
-    let func_sig = func.sig;
+    let func_sig = &func.sig;
     let func_ident = parse_rpc_method(&attr[0]);
-    let func_block = func.block;
+    let func_block = &func.block;
+    let func_output = &func_sig.output;
+    let func_return = PinBoxFutRet::parse(func_output);
+    let func_ret_ty = match func_output {
+        ReturnType::Default => quote! { () },
+        ReturnType::Type(_, ty) => quote! { #ty },
+    };
+    let ret_ty = func_return.ret_ty;
+
+    let func_block_wrapper = if func_return.is_pin_box_fut {
+        quote! {
+            Box::pin(async move {
+                let inst = std::time::Instant::now();
+
+                let ret: #ret_ty = #func_block.await;
+
+                if ret.is_err() {
+                    common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
+                        .#func_ident
+                        .failure
+                        .inc();
+                    return ret;
+                }
+
+                common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
+                    .#func_ident
+                    .success
+                    .inc();
+
+                common_apm::metrics::api::API_REQUEST_TIME_HISTOGRAM_STATIC
+                    .#func_ident
+                    .observe(common_apm::metrics::duration_to_sec(inst.elapsed()));
+
+                ret
+            })
+        }
+    } else {
+        quote! {
+            let inst = std::time::Instant::now();
+
+                let ret: #func_ret_ty = #func_block;
+
+                if ret.is_err() {
+                    common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
+                        .#func_ident
+                        .failure
+                        .inc();
+                    return ret;
+                }
+
+                common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
+                    .#func_ident
+                    .success
+                    .inc();
+
+                common_apm::metrics::api::API_REQUEST_TIME_HISTOGRAM_STATIC
+                    .#func_ident
+                    .observe(common_apm::metrics::duration_to_sec(inst.elapsed()));
+
+                ret
+        }
+    };
 
     quote! {
         #func_sig {
-            let inst = std::time::Instant::now();
-            let ret = #func_block;
-
-            if ret.is_err() {
-                common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
-                    .#func_ident
-                    .failure
-                    .inc();
-                return ret;
-            }
-
-            common_apm::metrics::api::API_REQUEST_RESULT_COUNTER_VEC_STATIC
-                .#func_ident
-                .success
-                .inc();
-
-            common_apm::metrics::api::API_REQUEST_TIME_HISTOGRAM_STATIC
-                .#func_ident
-                .observe(common_apm::metrics::duration_to_sec(inst.elapsed()));
-
-            ret
+            #func_block_wrapper
         }
     }
     .into()
