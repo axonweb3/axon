74d8425657f6240ba0c6a31e99d3807d827cc725
KaoImin <kaoimin@qq.com>
2022-03-08 19:51:50 +0800

    add system script classifier

--- core/executor/src/system.rs
+++ core/executor/src/system.rs
@@ -2,7 +2,9 @@ use evm::{ExitRevert, ExitSucceed};
 
 use protocol::codec::ProtocolCodec;
 use protocol::traits::{ApplyBackend, Backend};
-use protocol::types::{Apply, Basic, ExitReason, SignedTransaction, TxResp, H160, U256};
+use protocol::types::{
+    Apply, Basic, ExitReason, SignedTransaction, TransactionAction, TxResp, H160, U256,
+};
 
 pub const NATIVE_TOKEN_ISSUE_ADDRESS: H160 = H160::zero();
 
@@ -19,48 +21,66 @@ impl SystemExecutor {
         backend: &mut B,
         tx: SignedTransaction,
     ) -> TxResp {
-        let tx = tx.transaction.unsigned;
+        match classify_script(&tx.transaction.unsigned.action) {
+            SystemScriptCategory::NativeToken => call_native_token(backend, tx),
+        }
+    }
+}
 
-        if tx.data.len() < 21 || tx.data[0] > 1 {
+enum SystemScriptCategory {
+    NativeToken,
+}
+
+fn call_native_token<B: Backend + ApplyBackend>(backend: &mut B, tx: SignedTransaction) -> TxResp {
+    let tx = tx.transaction.unsigned;
+
+    if tx.data.len() < 21 || tx.data[0] > 1 {
+        return revert_resp(tx.gas_limit);
+    }
+
+    let direction = tx.data[0] == 0u8;
+    let l2_addr = H160::from_slice(&tx.data[1..21]);
+    let mut account = backend.basic(l2_addr);
+
+    if direction {
+        account.balance += tx.value;
+    } else {
+        if account.balance < tx.value {
             return revert_resp(tx.gas_limit);
         }
 
-        let direction = tx.data[0] == 0u8;
-        let l2_addr = H160::from_slice(&tx.data[1..21]);
-        let mut account = backend.basic(l2_addr);
-        if direction {
-            account.balance += tx.value;
-        } else {
-            if account.balance < tx.value {
-                return revert_resp(tx.gas_limit);
-            }
+        account.balance -= tx.value;
+    }
 
-            account.balance -= tx.value;
-        }
+    backend.apply(
+        vec![Apply::Modify {
+            address:       l2_addr,
+            basic:         Basic {
+                balance: account.balance,
+                nonce:   account.nonce,
+            },
+            code:          None,
+            storage:       vec![],
+            reset_storage: false,
+        }],
+        vec![],
+        false,
+    );
 
-        backend.apply(
-            vec![Apply::Modify {
-                address:       l2_addr,
-                basic:         Basic {
-                    balance: account.balance,
-                    nonce:   account.nonce,
-                },
-                code:          None,
-                storage:       vec![],
-                reset_storage: false,
-            }],
-            vec![],
-            false,
-        );
+    TxResp {
+        exit_reason:  ExitReason::Succeed(ExitSucceed::Returned),
+        ret:          account.balance.encode().unwrap().to_vec(),
+        gas_used:     1u64,
+        remain_gas:   (tx.gas_limit - 1u64).as_u64(),
+        logs:         vec![],
+        code_address: None,
+    }
+}
 
-        TxResp {
-            exit_reason:  ExitReason::Succeed(ExitSucceed::Returned),
-            ret:          account.balance.encode().unwrap().to_vec(),
-            gas_used:     1u64,
-            remain_gas:   (tx.gas_limit - 1u64).as_u64(),
-            logs:         vec![],
-            code_address: None,
-        }
+fn classify_script(action: &TransactionAction) -> SystemScriptCategory {
+    match action {
+        TransactionAction::Call(_addr) => SystemScriptCategory::NativeToken,
+        TransactionAction::Create => unreachable!(),
     }
 }
 
