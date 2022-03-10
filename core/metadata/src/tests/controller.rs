97aefb5539065c552dfae65b9991d8b910a61626
KaoImin <kaoimin@qq.com>
2022-03-10 16:39:56 +0800

    feat: add unit test

--- core/metadata/src/tests/controller.rs
+++ core/metadata/src/tests/controller.rs
@@ -2,26 +2,19 @@ use protocol::{tokio, traits::MetadataControl};
 
 use super::*;
 
-// #[test]
-// fn test_2() {
-// 	use ethers::core::abi::AbiEncode;
-
-//     let r =
-// BufReader::new(File::open("../../devtools/chain/metadata.json").unwrap());
-//     let metadata: Metadata = serde_json::from_reader(r).unwrap();
-// 	let call = abi::MetadataContractCalls::AppendMetadata(abi::AppendMetadataCall
-// { 		metadata: metadata.into(),
-// 	});
-// 	let raw_call = call.encode();
-// 	println!("{:?}", raw_call);
-//  }
-
 #[tokio::test(flavor = "multi_thread")]
-async fn test_1() {
+async fn test_get_metadata() {
     let handle = TestHandle::new().await;
     let ctl = handle.metadata_controller(100);
-    let res = ctl.get_metadata(Context::new(), &mock_header(0, handle.state_root));
-    println!("{:?}", res);
+    let res = ctl
+        .get_metadata(Context::new(), &mock_header(0, handle.state_root))
+        .unwrap();
+
+    assert_eq!(res.epoch, 0);
+    assert_eq!(res.version.start, 0);
+    assert_eq!(res.version.end, 999999);
+    assert_eq!(res.verifier_list.len(), 1);
+    assert_eq!(res.interval, 3000);
 }
 
 #[test]
@@ -35,15 +28,20 @@ fn test_calc_epoch() {
     assert_eq!(calc_epoch(200), 2);
 }
 
-#[test]
-fn metadata_address() {
-    let sender = H160::from_slice(
-        &protocol::codec::hex_decode("8ab0cf264df99d83525e9e11c7e4db01558ae1b1").unwrap(),
-    );
-    let nonce: U256 = 4u64.into();
-    let addr: H160 = core_executor::code_address(&sender, &nonce).into();
-    println!("{:?}", addr)
-}
+// #[test]
+// fn test_output_metadata() {
+//     use ethers::core::abi::AbiEncode;
+
+//     let r =
+// BufReader::new(File::open("../../devtools/chain/metadata.json").unwrap());
+//     let metadata: Metadata = serde_json::from_reader(r).unwrap();
+//     let call =
+// abi::MetadataContractCalls::AppendMetadata(abi::AppendMetadataCall {
+//         metadata: metadata.into(),
+//     });
+//     let raw_call = call.encode();
+//     println!("{:?}", raw_call);
+// }
 
 // #[test]
 // fn test_abi() {
