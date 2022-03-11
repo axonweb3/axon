b0c8bc3c5c0a0a157ab795beca28a3c94503cf31
root <root@DESKTOP-6P16ELT.localdomain>
2022-03-11 10:35:14 +0800

    optimization notes

--- tests/e2e/src/eth_getStorageAt.test.js
+++ tests/e2e/src/eth_getStorageAt.test.js
@@ -9,10 +9,11 @@ describe("Metamask", () => {
       chainId: goto.axonRpc.chianId,
     });
   });
+
   /**
   * param1:0x295a70b2de5e3953354a6a8344e616ed314d7253 // fixme
   * param2:0x0 // fixme
-  * param2:latest // fixme
+  * param3:latest // fixme
   */
   test("eth_getStorageAt_1", async () => {
     await goto.goto(page, pageName);
@@ -20,7 +21,7 @@ describe("Metamask", () => {
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
-    await testType.type("1");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("1");// 0: none params  1：common params to request   2: more parameter
     await param1.type("0x295a70b2de5e3953354a6a8344e616ed314d7253");
     await param2.type("0x0");
     await param3.type("latest");
@@ -30,7 +31,7 @@ describe("Metamask", () => {
   /**
   * param1:0x295a70b2de5e3953354a6a8344e616ed314d7252
   * param2:0x0
-  * param2:latest
+  * param3:latest
   */
   test("eth_getStorageAt_2", async () => {
     await goto.goto(page, pageName);
@@ -38,7 +39,7 @@ describe("Metamask", () => {
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
-    await testType.type("1");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("1");// 0: none params  1：common params to request   2: more parameter
     await param1.type("0x295a70b2de5e3953354a6a8344e616ed314d7252");
     await param2.type("0x0");
     await param3.type("latest");
@@ -48,7 +49,7 @@ describe("Metamask", () => {
   /**
   * param1:0x95a70b2de5e3953354a6a8344e616ed314d7252
   * param2:0x0
-  * param2:latest
+  * param3:latest
   */
   test("eth_getStorageAt_3", async () => {
     await goto.goto(page, pageName);
@@ -56,7 +57,7 @@ describe("Metamask", () => {
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
-    await testType.type("1");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("1");// 0: none params  1：common params to request   2: more parameter
     await param1.type("0x95a70b2de5e3953354a6a8344e616ed314d7252");
     await param2.type("0x0");
     await param3.type("latest");
@@ -66,7 +67,7 @@ describe("Metamask", () => {
   /**
   * param1:0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1
   * param2:0x0
-  * param2:0xfffffffff
+  * param3:0xfffffffff
   */
   test("eth_getStorageAt_4", async () => {
     await goto.goto(page, pageName);
@@ -74,7 +75,7 @@ describe("Metamask", () => {
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
-    await testType.type("1");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("1");// 0: none params  1：common params to request   2: more parameter
     await param1.type("0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1");
     await param2.type("0x0");
     await param3.type("0xfffffffff");
@@ -84,46 +85,26 @@ describe("Metamask", () => {
   /**
   * param1:0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1
   * param2:0x0
-  * param2:none
+  * param3:none
   */
   test("eth_getStorageAt_5", async () => {
     await goto.goto(page, pageName);
     const testType = await page.$(goto.pageIds.testTypeId);
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
-    await testType.type("3");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("3");// 0: none params  1：common params to request   2: more parameter 3:less parameter
     await param1.type("0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1");
     await param2.type("0x0");
     await goto.check(page, "Ox0");
   });
 
   /**
-  * param1:none
-  * param2:none
-  * param2:none
+  * none param
   */
   test("eth_getStorageAt_6", async () => {
     await goto.goto(page, pageName);
     const testType = await page.$(goto.pageIds.testTypeId);
-    await testType.type("0");// 0: none params  1：common params to request   2: dvantage parameter
+    await testType.type("0");// 0: none params  1：common params to request   2: more parameter
     await goto.check(page, "incorrect number of arguments");
-  });
-
-  /**
-  * param1: 0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1
-  * param2:0x0
-  * param2:latest
-  */
-  test("eth_getStorageAt_7", async () => {
-    await goto.goto(page, pageName);
-    const testType = await page.$(goto.pageIds.testTypeId);
-    const param1 = await page.$(goto.pageIds.param1Id);
-    const param2 = await page.$(goto.pageIds.param2Id);
-    const param3 = await page.$(goto.pageIds.param3Id);
-    await testType.type("2");// 0: none params  1：common params to request   2: dvantage parameter
-    await param1.type("0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1");
-    await param2.type("0x0");
-    await param3.type("latest");
-    await goto.check(page, "-32001");
   });
 });
--- tests/e2e/src/goto.js
+++ tests/e2e/src/goto.js
@@ -10,7 +10,7 @@ const goto = async (currentpage, pageName) => {
       currentpage.bringToFront(),
     ]);
   } catch (ex) {
-    throw new Error(ex);
+    console.log(ex);
   }
 };
 
