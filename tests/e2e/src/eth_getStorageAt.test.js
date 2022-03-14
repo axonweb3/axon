ba31657f14ea4e626d386489e2a015db7ea0a2c7
root <root@DESKTOP-6P16ELT.localdomain>
2022-03-14 12:41:06 +0800

    adjust test parameters

--- tests/e2e/src/eth_getStorageAt.test.js
+++ tests/e2e/src/eth_getStorageAt.test.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line
 import { goto } from "./goto";
 import { Config } from "../config";
+// eslint-disable-next-line import/named
+import { testDataManage } from "./create_test_data/createTestDataManage";
 
+let testDataInfo = null;
 const pageName = "eth_getStorageAt.html";
 describe("Metamask", () => {
   beforeAll(async () => {
@@ -10,11 +13,12 @@ describe("Metamask", () => {
       rpc: Config.getIns().axonRpc.url,
       chainId: Config.getIns().axonRpc.chainId,
     });
+    testDataInfo = await testDataManage.readTestDataAsJson("testData_1.json");
   });
   /**
-  * param1:0x295a70b2de5e3953354a6a8344e616ed314d7253 // fixme
-  * param2:0x0 // fixme
-  * param3:latest // fixme
+  * param1:real address
+  * param2:0x0
+  * param3:real block number
   */
   test("eth_getStorageAt_1", async () => {
     await goto.goto(page, pageName);
@@ -23,16 +27,16 @@ describe("Metamask", () => {
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
     await testType.type("1");// 0: none params  1：common params to request   2: more parameter
-    await param1.type("0x295a70b2de5e3953354a6a8344e616ed314d7253");
+    await param1.type(testDataInfo.contractAddress);
     await param2.type("0x0");
-    await param3.type("latest");
-    await goto.check(page, "0x0");
+    await param3.type(`0x${testDataInfo.blockNumber.toString(16)}`);
+    await goto.check(page, "TT");
   });
 
   /**
-  * param1:0x295a70b2de5e3953354a6a8344e616ed314d7252
+  * param1:not exist address
   * param2:0x0
-  * param3:latest
+  * param3:real block number
   */
   test("eth_getStorageAt_2", async () => {
     await goto.goto(page, pageName);
@@ -43,13 +47,13 @@ describe("Metamask", () => {
     await testType.type("1");// 0: none params  1：common params to request   2: more parameter
     await param1.type("0x295a70b2de5e3953354a6a8344e616ed314d7252");
     await param2.type("0x0");
-    await param3.type("latest");
-    await goto.check(page, "0x0");
+    await param3.type(`0x${testDataInfo.blockNumber.toString(16)}`);
+    await goto.check(page, "Can't find this address");
   });
 
   /**
-  * param1:0x95a70b2de5e3953354a6a8344e616ed314d7252
-  * param2:0x0
+  * param1:real address
+  * param2:0xfffff, that is not exist position
   * param3:latest
   */
   test("eth_getStorageAt_3", async () => {
@@ -59,16 +63,16 @@ describe("Metamask", () => {
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
     await testType.type("1");// 0: none params  1：common params to request   2: more parameter
-    await param1.type("0x95a70b2de5e3953354a6a8344e616ed314d7252");
-    await param2.type("0x0");
-    await param3.type("latest");
-    await goto.check(page, "0x0");
+    await param1.type(testDataInfo.contractAddress);
+    await param2.type("0xfffff");
+    await param3.type(`0x${testDataInfo.blockNumber.toString(16)}`);
+    await goto.check(page, "Can't find this position");
   });
 
   /**
-  * param1:0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1
+  * param1:real address
   * param2:0x0
-  * param3:0xfffffffff
+  * param3:0xfffffffff, that is not exist block number
   */
   test("eth_getStorageAt_4", async () => {
     await goto.goto(page, pageName);
@@ -77,14 +81,14 @@ describe("Metamask", () => {
     const param2 = await page.$(goto.pageIds.param2Id);
     const param3 = await page.$(goto.pageIds.param3Id);
     await testType.type("1");// 0: none params  1：common params to request   2: more parameter
-    await param1.type("0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1");
+    await param1.type(testDataInfo.contractAddress);
     await param2.type("0x0");
     await param3.type("0xfffffffff");
     await goto.check(page, "Can't find this block");
   });
 
   /**
-  * param1:0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1
+  * param1:real address
   * param2:0x0
   * param3:none
   */
@@ -94,9 +98,9 @@ describe("Metamask", () => {
     const param1 = await page.$(goto.pageIds.param1Id);
     const param2 = await page.$(goto.pageIds.param2Id);
     await testType.type("3");// 0: none params  1：common params to request   2: more parameter 3:less parameter
-    await param1.type("0x8ab0CF264DF99D83525e9E11c7e4db01558AE1b1");
+    await param1.type(testDataInfo.contractAddress);
     await param2.type("0x0");
-    await goto.check(page, "Ox0");
+    await goto.check(page, "-32603");
   });
 
   /**
