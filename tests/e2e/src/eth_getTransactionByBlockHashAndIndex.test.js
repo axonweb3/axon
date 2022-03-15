1ac72d70d200a4782d97c87782df8a735e1ad5cf
root <root@DESKTOP-6P16ELT.localdomain>
2022-03-15 20:24:48 +0800

    update annotation

--- tests/e2e/src/eth_getTransactionByBlockHashAndIndex.test.js
+++ tests/e2e/src/eth_getTransactionByBlockHashAndIndex.test.js
@@ -18,7 +18,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   const param2 = await page.$(goto.pageIds.param2Id);
-  await testType.type("1");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("1");// 0: none params  1：common params to request   2: more params
   await param1.type(testDataInfo.blockHash);
   await param2.type(testDataInfo.transactionIndex.toString());
   await goto.check(page, testDataInfo.blockHash);
@@ -33,7 +33,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   const param2 = await page.$(goto.pageIds.param2Id);
-  await testType.type("1");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("1");// 0: none params  1：common params to request   2: more params
   await param1.type(testDataInfo.blockHash);
   await param2.type("0xffff");
   await goto.check(page, "null");
@@ -48,7 +48,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   const param2 = await page.$(goto.pageIds.param2Id);
-  await testType.type("1");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("1");// 0: none params  1：common params to request   2: more params
   await param1.type("0x8f49fdb32d8b4ce4bb85c9f29fa06badcea78737d8b40fef0d2813c060524815");
   await param2.type(testDataInfo.transactionIndex.toString());
   await goto.check(page, "null");
@@ -63,7 +63,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   const param2 = await page.$(goto.pageIds.param2Id);
-  await testType.type("1");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("1");// 0: none params  1：common params to request   2: more params
   await param1.type("0x8f49fdb32d8b4ce4bb80c9f29fa06badcea787");
   await param2.type(testDataInfo.transactionIndex.toString());
   await goto.check(page, "-32602");
@@ -78,7 +78,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   await param1.type(testDataInfo.blockHash);
-  await testType.type("3");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("3");// 0: none params  1：common params to request   2: more params
   await goto.check(page, "-32602");
 });
 
@@ -89,7 +89,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
 it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   await goto.goto(page, pageName);
   const testType = await page.$(goto.pageIds.testTypeId);
-  await testType.type("0");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("0");// 0: none params  1：common params to request   2: more params
   await goto.check(page, "-32602");
 });
 
@@ -102,7 +102,7 @@ it("eth_getTransactionByBlockHashAndIndex_1", async () => {
   const testType = await page.$(goto.pageIds.testTypeId);
   const param1 = await page.$(goto.pageIds.param1Id);
   const param2 = await page.$(goto.pageIds.param2Id);
-  await testType.type("2");// 0: none params  1：common params to request   2: more parameter
+  await testType.type("2");// 0: none params  1：common params to request   2: more params
   await param1.type(testDataInfo.blockHash);
   await param2.type(testDataInfo.transactionIndex.toString());
   await goto.check(page, "-32602");
