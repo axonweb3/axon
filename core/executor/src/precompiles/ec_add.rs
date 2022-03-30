8b57645a120ff1b2a1f45c6cfd398d706393a5eb
KaoImin <kaoimin@qq.com>
2022-03-30 12:35:11 +0800

    feat: add precompile contracts

---
+++ core/executor/src/precompiles/ec_add.rs
@@ -0,0 +1,54 @@
+use bn::AffineG1;
+use evm::executor::stack::{PrecompileFailure, PrecompileOutput};
+use evm::{Context, ExitError, ExitSucceed};
+
+use protocol::types::H160;
+
+use crate::err;
+use crate::precompiles::{precompile_address, read_point, PrecompileContract};
+
+#[derive(Default)]
+pub struct EcAdd;
+
+impl PrecompileContract for EcAdd {
+    const ADDRESS: H160 = precompile_address(0x06);
+    const MIN_GAS: u64 = 150;
+
+    fn exec_fn(
+        input: &[u8],
+        gas_limit: Option<u64>,
+        _context: &Context,
+        _is_static: bool,
+    ) -> Result<PrecompileOutput, PrecompileFailure> {
+        let gas = Self::gas_cost(input);
+        if let Some(limit) = gas_limit {
+            if limit < gas {
+                return err!();
+            }
+        }
+
+        let p1 = read_point(input, 0)?;
+        let p2 = read_point(input, 64)?;
+
+        let mut buf = [0u8; 64];
+        if let Some(sum) = AffineG1::from_jacobian(p1 + p2) {
+            sum.x()
+                .to_big_endian(&mut buf[0..32])
+                .map_err(|_| err!(_, "Invalid sum X"))?;
+            sum.y()
+                .to_big_endian(&mut buf[32..64])
+                .map_err(|_| err!(_, "Invalid sum Y"))?;
+        }
+
+        Ok(PrecompileOutput {
+            exit_status: ExitSucceed::Returned,
+            cost:        Self::MIN_GAS,
+            output:      buf.to_vec(),
+            logs:        vec![],
+        })
+    }
+
+    fn gas_cost(_input: &[u8]) -> u64 {
+        Self::MIN_GAS
+    }
+}
--- core/executor/src/precompiles/mod.rs
+++ core/executor/src/precompiles/mod.rs
@@ -1,4 +1,7 @@
 mod blake2_f;
+mod ec_add;
+mod ec_mul;
+mod ec_pairing;
 mod ecrecover;
 mod identity;
 mod modexp;
@@ -11,14 +14,37 @@ mod tests;
 
 use std::collections::BTreeMap;
 
+use bn::{AffineG1, Fq, Fr, Group, G1};
 use evm::executor::stack::{PrecompileFailure, PrecompileFn, PrecompileOutput};
-use evm::Context;
+use evm::{Context, ExitError};
 
 use protocol::types::H160;
 
 use crate::precompiles::{
-    ecrecover::EcRecover, identity::Identity, ripemd160::Ripemd160, sha256::Sha256,
+    blake2_f::Blake2F, ec_add::EcAdd, ec_mul::EcMul, ec_pairing::EcPairing, ecrecover::EcRecover,
+    identity::Identity, ripemd160::Ripemd160, sha256::Sha256,
 };
+
+#[macro_export]
+macro_rules! err {
+    () => {
+        Err(PrecompileFailure::Error {
+            exit_status: ExitError::OutOfGas,
+        })
+    };
+
+    ($msg: expr) => {
+        Err(PrecompileFailure::Error {
+            exit_status: ExitError::Other($msg.into()),
+        })
+    };
+
+    (_, $msg: expr) => {
+        PrecompileFailure::Error {
+            exit_status: ExitError::Other($msg.into()),
+        }
+    };
+}
 
 macro_rules! precompiles {
     () => { BTreeMap::new() };
@@ -54,5 +80,35 @@ const fn precompile_address(addr: u8) -> H160 {
 }
 
 pub fn build_precompile_set() -> BTreeMap<H160, PrecompileFn> {
-    precompiles!(EcRecover, Sha256, Ripemd160, Identity)
+    precompiles!(EcRecover, Sha256, Ripemd160, Identity, EcAdd, EcMul, EcPairing, Blake2F)
+}
+
+pub(crate) fn read_point(input: &[u8], start: usize) -> Result<G1, PrecompileFailure> {
+    if input.len() < start + 64 {
+        return err!("Invalid input length");
+    }
+
+    let px =
+        Fq::from_slice(&input[start..(start + 32)]).map_err(|_| err!(_, "Invalid X coordinate"))?;
+
+    let py = Fq::from_slice(&input[(start + 32)..(start + 64)])
+        .map_err(|_| err!(_, "Invalid Y coordinate"))?;
+
+    let ret = if px == Fq::zero() && py == Fq::zero() {
+        G1::zero()
+    } else {
+        AffineG1::new(px, py)
+            .map_err(|_| err!(_, "Invalid curve point"))?
+            .into()
+    };
+
+    Ok(ret)
+}
+
+pub(crate) fn read_fr(input: &[u8], start: usize) -> Result<Fr, PrecompileFailure> {
+    if input.len() < start + 32 {
+        return err!("Invalid input length");
+    }
+
+    Fr::from_slice(&input[start..(start + 32)]).map_err(|_| err!(_, "Invalid field element"))
 }
