5dfb827e60db6dd8af02e64001de1b3728593d7e
KaoImin <kaoimin@qq.com>
2022-05-25 14:41:15 +0800

    add some types

--- core/cross-client/src/types.rs
+++ core/cross-client/src/types.rs
@@ -1,17 +1,40 @@
 use serde::{Deserialize, Serialize};
 
-use protocol::types::Hash;
+use protocol::types::{Hash, H160};
+
+use crate::error::CrossChainError;
 
 #[repr(u8)]
-#[derive(Copy, Clone, Debug)]
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Direction {
     FromCkb = 0u8,
     FromAxon = 1u8,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
-pub struct FromCkbRequest {
-    pub ckb_tx_hash: Hash,
+impl TryFrom<u8> for Direction {
+    type Error = CrossChainError;
+
+    fn try_from(d: u8) -> Result<Self, Self::Error> {
+        match d {
+            0 => Ok(Direction::FromCkb),
+            1 => Ok(Direction::FromAxon),
+            _ => Err(CrossChainError::InvalidDirection),
+        }
+    }
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct Transfer {
+    pub direction:      Direction,
+    pub tx_hash:        Hash,
+    pub address:        H160,
+    pub erc20_address:  H160,
+    pub sudt_type_hash: Hash,
+    pub ckb_amount:     u64,
+    pub sudt_amount:    u128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct FromCkbRequest(pub Vec<Transfer>);
 
 pub struct FromAxonRequest {}
