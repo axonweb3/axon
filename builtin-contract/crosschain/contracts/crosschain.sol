8f6fd22e4915933b1966ac8d977fb8943e140fd9
daryl <sai9375@hotmail.com>
2022-05-26 18:41:18 +0800

    feat(crosschain): cross token to ckb.

--- builtin-contract/crosschain/contracts/crosschain.sol
+++ builtin-contract/crosschain/contracts/crosschain.sol
@@ -1,34 +1,134 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0;
+pragma solidity >=0.8.0;
 
 // import "hardhat/console.sol";
+import {IMirrorToken} from "./MirrorToken.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
-contract CrossChain {
-    struct Layer1Asset {
-        bytes32 udt_hash;
-        bytes32 cross_chain_tx_hash;
-        address erc20_address;
+contract CrossChain is AccessControlEnumerable, EIP712 {
+    bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
+
+    address public constant AT_ADDRESS = address(0);
+    uint256 private _relayerThreshold;
+    address private _metadata;
+    address private _wCKB;
+    uint256 private _minWCKB;
+
+    mapping(address => TokenConfig) private _tokenConfigs;
+    mapping(address => bool) private _mirrorTokens;
+
+    event CrossToCKB(
+        bytes32 lockscript,
+        address token,
+        uint256 amount,
+        uint256 minWCKBAmount
+    );
+
+    struct TokenConfig {
+        uint256 feeRatio;
+        uint256 threshold;
     }
 
-    // to store all layer1 assets with erc20 as key
-    mapping(address => Layer1Asset) assets;
-
-    // push new layer1 asset into `assets`
-    function insert(address erc20_address, Layer1Asset memory asset) public {
-        require(
-            erc20_address == asset.erc20_address, 
-            "fatal/mismatched erc20 address"
-        );
-        assets[erc20_address] = asset;
+    struct AxonToCKBREcord {
+        bytes32 lockscriptHash;
+        address tokenAddress;
+        uint256 amount;
     }
 
-    // get layer1 asset from `assets` by erc20
-    function get(address erc20_address) public view returns (Layer1Asset memory) {
+    constructor(
+        address[] memory relayers,
+        uint256 threshold,
+        address metadata,
+        string memory name,
+        string memory version
+    ) EIP712(name, version) {
+        uint256 length = relayers.length;
+        for (uint256 i = 0; i < length; ++i) {
+            _setupRole(RELAYER_ROLE, relayers[i]);
+        }
+        _setRoleAdmin(RELAYER_ROLE, RELAYER_ROLE);
+
+        _relayerThreshold = threshold;
+        _metadata = metadata;
+    }
+
+    function setTokenConfig(address token, TokenConfig calldata config)
+        external
+    {
         require(
-            assets[erc20_address].erc20_address == erc20_address,
-            "fatal/non-indexed erc20 address"
+            hasRole(RELAYER_ROLE, _msgSender()),
+            "CrossChain: must have relayer role"
         );
-        return assets[erc20_address];
+
+        _tokenConfigs[token] = config;
+    }
+
+    function setWCKB(address token) external {
+        require(
+            hasRole(RELAYER_ROLE, _msgSender()),
+            "CrossChain: must have relayer role"
+        );
+
+        _wCKB = token;
+    }
+
+    function setWCKBMin(uint256 amount) external {
+        require(
+            hasRole(RELAYER_ROLE, _msgSender()),
+            "CrossChain: must have relayer role"
+        );
+
+        _minWCKB = amount;
+    }
+
+    function addMirrorToken(address token) public {
+        require(
+            hasRole(RELAYER_ROLE, _msgSender()),
+            "CrossChain: must have relayer role"
+        );
+
+        _mirrorTokens[token] = true;
+    }
+
+    function isMirrorToken(address token) public view returns (bool) {
+        return _mirrorTokens[token];
+    }
+
+    function lockAT(bytes32 lockscript) external payable {
+        require(msg.value > 0, "CrossChain: value must be more than 0");
+
+        IERC20(_wCKB).transferFrom(_msgSender(), address(this), _minWCKB);
+
+        emit CrossToCKB(lockscript, address(0), msg.value, _minWCKB);
+    }
+
+    function crossTokenToCKB(
+        bytes32 lockscript,
+        address token,
+        uint256 amount
+    ) external {
+        require(amount > 0, "CrossChain: amount must be more than 0");
+
+        require(
+            IERC20(_wCKB).balanceOf(_msgSender()) > _minWCKB,
+            "CrossChain: amount of wckb is insufficient"
+        );
+
+        if (isMirrorToken(token)) {
+            if (token == _wCKB) {
+                amount -= _minWCKB;
+            }
+
+            IMirrorToken(token).burn(_msgSender(), amount);
+        } else {
+            IERC20(token).transferFrom(_msgSender(), address(this), amount);
+        }
+
+        IERC20(_wCKB).transferFrom(_msgSender(), address(this), _minWCKB);
+
+        emit CrossToCKB(lockscript, token, amount, _minWCKB);
     }
 }
--- builtin-contract/crosschain/test/crosschain.js
+++ builtin-contract/crosschain/test/crosschain.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
+const { deployMockContract } = require('@ethereum-waffle/mock-contract');
 
 function hexToBytes(hex) {
     for (var bytes = [], c = 0; c < hex.length; c += 2)
@@ -7,40 +8,306 @@ function hexToBytes(hex) {
     return bytes;
 }
 
+async function deployMirrorToken(owner) {
+    const MirrorToken = await ethers.getContractFactory('MirrorToken');
+    contract = await MirrorToken.connect(owner).deploy("CKB", "CKB");
+
+    return contract;
+}
+
+async function deployTestToken(owner) {
+    const TestToken = await ethers.getContractFactory('TestToken')
+    contract = await TestToken.connect(owner).deploy();
+
+    return contract;
+}
+
 describe("CrossChain", () => {
-    let wallets = null
-    let contract = null
-    let layer1asset = {
-        udt_hash: hexToBytes("63b9fe46a9217a85203fc0cd7b67f3238ec93889d21fefb7cf11d40a1c3ddd9c"),
-        cross_chain_tx_hash: hexToBytes("63b9fe46a9217a85203fc0cd7b67f3238ec93889d21fefb7cf11d40a1c3ddd9c"),
-        erc20_address: ""
-    }
+    let owner = null;
+    let wallets = null;
+    let contract = null;
+    let wckb = null;
+    let mirrorToken = null;
+    let simpleToken = null;
+    let ATAddress = `0x${'0'.repeat(40)}`;
+    let lockscript = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('kk'));
 
-    before(async () => {
-        let deployer = await ethers.getContractFactory("CrossChain")
-        contract = await deployer.deploy()
-        await contract.deployed()
-        wallets = await ethers.getSigners()
-        layer1asset.erc20_address = wallets[0].address
-    })
+    beforeEach(async () => {
+        [owner, ...wallets] = await ethers.getSigners();
 
-    it("should fail on inserting parameters with mismatched erc20_address", async () => {
-        await expect(contract.insert(wallets[1].address, layer1asset))
-            .to.be.revertedWith("fatal/mismatched erc20 address")
-    })
+        let deployer = await ethers.getContractFactory("CrossChain");
+        contract = await deployer.connect(owner).deploy([owner.address, wallets[0].address, wallets[1].address, wallets[2].address], 3, wallets[5].address, '', '');
+        await contract.deployed();
 
-    it("should succeed on inserting valid parameters", async () => {
-        await expect(contract.insert(wallets[0].address, layer1asset))
-            .to.be.not.reverted
-    })
+        await contract.connect(owner).setWCKBMin(1);
 
-    it("should fail on getting asset by non-indexed erc20 address", async () => {
-        await expect(contract.get(wallets[1].address))
-            .to.be.revertedWith("fatal/non-indexed erc20 address")
-    })
+        wckb = await deployMirrorToken(owner);
+        await contract.connect(owner).setWCKB(wckb.address);
+        await contract.connect(owner).addMirrorToken(wckb.address);
+        await contract.connect(owner).setTokenConfig(wckb.address, [10, 100]);
 
-    it("should succeed on getting asset by indexed erc20 address", async () => {
-        await expect(contract.get(wallets[0].address))
-            .to.be.not.reverted
-    })
-})
+        mirrorToken = await deployMirrorToken(owner);
+        await contract.connect(owner).addMirrorToken(mirrorToken.address);
+        await contract.connect(owner).setTokenConfig(mirrorToken.address, [10, 100]);
+
+        simpleToken = await deployTestToken(owner);
+        await contract.connect(owner).setTokenConfig(simpleToken.address, [10, 100]);
+
+        await contract.connect(owner).setTokenConfig(ATAddress, [10, 100]);
+    });
+
+    it("user lock AT should success", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).lockAT(lockscript, { value: 100 }))
+            .emit(contract, 'CrossToCKB')
+            .withArgs(lockscript, ATAddress, 100, 1);
+
+        expect(await wckb.balanceOf(contract.address)).equal(1);
+        expect(await ethers.provider.getBalance(contract.address)).equal(100);
+    });
+
+    it("user lock AT should fail while value is zero", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.lockAT(lockscript)).revertedWith('CrossChain: value must be more than 0');
+    });
+
+    it("user lock AT should fail while the wckb is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).lockAT(lockscript, { value: 100 }))
+            .revertedWith('ERC20: transfer amount exceeds balance');
+    });
+
+    it("user lock Token should success", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await simpleToken.connect(owner).transfer(wallet.address, 1000);
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await simpleToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, simpleToken.address, 100))
+            .emit(contract, 'CrossToCKB')
+            .withArgs(lockscript, simpleToken.address, 100, 1);
+
+        expect(await wckb.balanceOf(contract.address)).equal(1);
+        expect(await simpleToken.balanceOf(contract.address)).equal(100);
+    });
+
+    it("user lock Token should fail while the amount is 0", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await simpleToken.connect(owner).transfer(wallet.address, 1000);
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await simpleToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, simpleToken.address, 0))
+            .revertedWith('CrossChain: amount must be more than 0');
+
+    });
+
+    it("user lock token should fail while amount is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await simpleToken.connect(owner).transfer(wallet.address, 10);
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await simpleToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, simpleToken.address, 100))
+            .revertedWith('ERC20: transfer amount exceeds balance');
+    });
+
+    it("user lock Token should fail while wckb is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await simpleToken.connect(owner).transfer(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await simpleToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, simpleToken.address, 100))
+            .revertedWith('CrossChain: amount of wckb is insufficient');
+    });
+
+    it("user burn ckb should success", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, wckb.address, 100))
+            .emit(contract, 'CrossToCKB')
+            .withArgs(lockscript, wckb.address, 99, 1);
+
+        expect(await wckb.balanceOf(contract.address)).equal(1);
+    });
+
+    it("user burn ckb should fail while wckb is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 10);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, wckb.address, 100))
+            .revertedWith('ERC20: burn amount exceeds balance');
+    });
+
+    it("user burn ckb should fail while wckb is not enough for extra amount", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 99);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, wckb.address, 100))
+            .revertedWith('ERC20: transfer amount exceeds balance');
+    });
+
+    it("user burn ckb should fail while amount is zero", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+        await wckb.connect(owner).mint(wallet.address, 1000);
+
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, wckb.address, 0))
+            .revertedWith('CrossChain: amount must be more than 0');
+    });
+
+    it("user burn mirror token should success", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+
+        await wckb.connect(owner).mint(wallet.address, 1000);
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await mirrorToken.connect(owner).mint(wallet.address, 1000);
+        await mirrorToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await mirrorToken.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, mirrorToken.address, 100))
+            .emit(contract, 'CrossToCKB')
+            .withArgs(lockscript, mirrorToken.address, 100, 1);
+
+        expect(await wckb.balanceOf(contract.address)).equal(1);
+        expect(await mirrorToken.balanceOf(wallet.address)).equal(900);
+    });
+
+    it("user burn mirror token should fail while amount is zero", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+
+        await wckb.connect(owner).mint(wallet.address, 1000);
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await mirrorToken.connect(owner).mint(wallet.address, 1000);
+        await mirrorToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await mirrorToken.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, mirrorToken.address, 0))
+            .revertedWith('CrossChain: amount must be more than 0');
+    });
+
+    it("user burn mirror token should fail while ckb is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+
+        await mirrorToken.connect(owner).mint(wallet.address, 1000);
+        await mirrorToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await mirrorToken.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, mirrorToken.address, 100))
+            .revertedWith('CrossChain: amount of wckb is insufficient');
+    });
+
+    it("user burn mirror token should fail while token is not enough", async () => {
+        const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+        await owner.sendTransaction({
+            to: wallet.address,
+            value: ethers.utils.parseEther('1.0'),
+        });
+
+        await wckb.connect(owner).mint(wallet.address, 1000);
+        await wckb.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await wckb.connect(owner).transferOwnership(contract.address);
+
+        await mirrorToken.connect(owner).mint(wallet.address, 1000);
+        await mirrorToken.connect(wallet).approve(contract.address, ethers.constants.MaxUint256);
+        await mirrorToken.connect(owner).transferOwnership(contract.address);
+
+        await expect(contract.connect(wallet).crossTokenToCKB(lockscript, mirrorToken.address, 100000))
+            .revertedWith('ERC20: burn amount exceeds balance');
+    });
+});
