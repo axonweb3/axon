48f06424a43aaf61a7a8a77d56b90beee9da9439
KaoImin <kaoimin@qq.com>
2022-06-09 14:58:07 +0800

    add some logs

--- core/cross-client/src/monitor.rs
+++ core/cross-client/src/monitor.rs
@@ -86,7 +86,15 @@ impl<C: CkbClient + 'static> CrossChainMonitor<C> {
             }
         }
 
-        self.req_tx.send(list).unwrap();
+        if !list.is_empty() {
+            log::info!(
+                "[cross-chain]: Cross from CKB block {:?}, request tx count {:?}",
+                (self.handle_ckb_number..=non_fork_tip), list.len()
+            );
+
+            self.req_tx.send(list).unwrap();
+        }
+
         self.handle_ckb_number = non_fork_tip + 1;
     }
 
