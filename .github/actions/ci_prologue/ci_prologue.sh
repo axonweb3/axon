f11e05a6da9423a916f4c878527a4b696090c5f5
Eason <kaoimin@qq.com>
2022-09-14 00:42:47 +0800

    add wip ci

--- .github/actions/ci_prologue/ci_prologue.sh
+++ .github/actions/ci_prologue/ci_prologue.sh
@@ -27,7 +27,7 @@ fun_pasing_message(){
   if [ $? -eq 0 ]; then
     job_run_list=`echo "${MESSAGE}"| grep "ci-runs-only" | awk -F ':' '{print $2}'`
   else
-    job_run_list=" [ Code Format,Chaos CI,Cargo Clippy,Coverage Test,E2E Tests,Conventional PR,Unit Tests,Web3 Compatible Tests,OCT 1-5 And 12-15,OCT 6-10,OCT 11,OCT 16-19,v3 Core Tests] "
+    job_run_list=" [ Code Format,Chaos CI,Cargo Clippy,Coverage Test,E2E Tests,PR Lint,Unit Tests,Web3 Compatible Tests,OCT 1-5 And 12-15,OCT 6-10,OCT 11,OCT 16-19,v3 Core Tests] "
   fi
   echo "job_run_list is ""$job_run_list"
 
@@ -49,7 +49,7 @@ if [[ $GITHUB_EVENT_NAME == "pull_request" ]];then
   fi
 fi
 if [[ $GITHUB_EVENT_NAME == "push" ]];then
-  job_run_list=" [ Code Format,Chaos CI,Cargo Clippy,Coverage Test,E2E Tests,Conventional PR,Unit Tests,Web3 Compatible Tests,OCT 1-5 And 12-15,OCT 6-10,OCT 11,OCT 16-19,v3 Core Tests] "
+  job_run_list=" [ Code Format,Chaos CI,Cargo Clippy,Coverage Test,E2E Tests,PR Lint,Unit Tests,Web3 Compatible Tests,OCT 1-5 And 12-15,OCT 6-10,OCT 11,OCT 16-19,v3 Core Tests] "
   #set reqiured output
   fun_jobs "$job_run_list"
 fi
