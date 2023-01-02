0569425a40fc06cded326c1d7302ddd7249b6577
cryptape.michael <default@email.com>
2023-01-03 01:54:20 +0800

    feat: update dir files
    update files

---
+++ core/executor/src/system_contract/election/error.rs
@@ -0,0 +1,5 @@
+
+#[derive(Debug, Display)]
+pub enum ElectionError {
+
+}
---
+++ core/executor/src/system_contract/election/exec.rs
@@ -0,0 +1,15 @@
+fn registerNode () {
+    // implement!()
+}
+
+fn vote () {
+    // implement!()
+}
+
+fn getVoteCount () {
+    // implement!()
+}
+
+fn getTotalVoteCount () {
+    // implement!()
+}
