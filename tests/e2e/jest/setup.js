b62bd246a1b26b7b1f491c58a2d81a265fd1b9eb
cryptape.michael <default@email.com>
2023-01-03 00:27:43 +0800

    fix(e2e): bump dappeteer version
    parent 6c3b0ae7423f8e2ba24876f72ee532c24283da2e
    author cryptape.michael <default@email.com> 1672676863 +0800
    committer cryptape.michael <default@email.com> 1672970389 +0800
    
    chore(e2e): bump dappeteer
    
    bump dappeteer to latest
    
    feat: change bootstrap
    
    feat: add seed
    
    feat: pump dappeteer
    
    feat: used for move os
    
    feat: use for other os
    
    feat: use for other os
    
    feat: use for other os
    
    feat: add to linux
    
    feat: update debug
    
    feat: success code
    
    fit(e2e): bump dappeteer from v2.x to v3.x
    
    fix: fix test case
    
    chore(deps): bump arc-swap from 1.5.1 to 1.6.0 (#954)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    chore(deps): bump lru from 0.8.1 to 0.9.0 (#956)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    style: add comment
    
    style: update comment
    
    feat: only success for non_headless
    
    fix: fix headless mode
    
    fix(e2e): fix dappeteer compatible with revert stable version  (#963)
    
    Co-authored-by: michael.debian <michael@debian.com>
    
    chore: bump puppeteer version
    
    chore(deps): bump arc-swap from 1.5.1 to 1.6.0 (#954)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    chore(deps): bump lru from 0.8.1 to 0.9.0 (#956)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    fix(e2e): fix dappeteer compatible with revert stable version  (#963)
    
    Co-authored-by: michael.debian <michael@debian.com>
    
    fix: bump dappeteer version

--- tests/e2e/jest/setup.js
+++ tests/e2e/jest/setup.js
@@ -1,41 +1,43 @@
 import puppeteer from "puppeteer";
-import { launch, setupMetamask, getMetamaskWindow } from "@chainsafe/dappeteer";
+import bootstrap from "@chainsafe/dappeteer";
+import {RECOMMENDED_METAMASK_VERSION} from "@chainsafe/dappeteer/dist/index";
 
 import Config from "../config";
 import createTransactionData from "../src/create_test_data/createTestDataManage";
 
-export const DAPPETEER_DEFAULT_CONFIG = {
-  metamaskVersion: "v10.10.2",
+export const MetaMaskOptions = {
+  // empty wallet
+  seed: "bubble young armed shed unusual acid pilot chase caught crop defense only",
+  password: "12345678",
+  metamaskVersion: RECOMMENDED_METAMASK_VERSION,
   args: [
-    process.env.HEADLESS ? "--headless=chrome" : "",
+    process.env.HEADLESS ? "--headless=new" : "",
     process.env.WSL ? "-no-sandbox" : "",
-  ],
+  ]
 };
 export default async function setup() {
-  const browser = await launch(puppeteer, DAPPETEER_DEFAULT_CONFIG);
+  const [metaMask, ,browser] = await bootstrap(puppeteer, MetaMaskOptions);
   try {
     await createTransactionData.resetTestTmpFiles();
     await createTransactionData.createTransactionData(); // create test data
-    await setupMetamask(browser, {});
     global.browser = browser;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
     throw error;
   }
+  
   process.env.PUPPETEER_WS_ENDPOINT = browser.wsEndpoint();
-
   global.browser = browser;
-  global.metamask = await getMetamaskWindow(browser);
+  global.metamask = metaMask;
 
   // await Config.getIns().initialize();
   await metamask.addNetwork({
     networkName: Config.getIns().axonRpc.netWorkName,
     rpc: Config.getIns().axonRpc.url,
     chainId: Config.getIns().axonRpc.chainId,
-    // chainId: 5,
+    symbol: "AXON"
   });
-
   const page = await browser.newPage();
   await page.bringToFront();
   global.page = page;
