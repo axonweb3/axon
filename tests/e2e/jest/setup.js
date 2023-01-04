0ef17887d11b1360e772526de85ad1bc38218c1b
cryptape.michael <default@email.com>
2023-01-04 09:43:04 +0800

    feat: change bootstrap

--- tests/e2e/jest/setup.js
+++ tests/e2e/jest/setup.js
@@ -1,22 +1,43 @@
 import puppeteer from "puppeteer";
-import { launch, setupMetamask, getMetamaskWindow } from "@chainsafe/dappeteer";
+import { bootstrap,launch, setupMetamask, getMetamaskWindow } from "@chainsafe/dappeteer";
 
 import Config from "../config";
 import createTransactionData from "../src/create_test_data/createTestDataManage";
 
+// export const DAPPETEER_DEFAULT_CONFIG = {
+//   metamaskVersion: "v10.23.2",
+//   args: [
+//     process.env.HEADLESS ? "--headless=chrome" : "",
+//     process.env.WSL ? "-no-sandbox" : "",
+//   ],
+// };
 export const DAPPETEER_DEFAULT_CONFIG = {
-  metamaskVersion: "v10.23.2",
+  metamaskVersion: "v10.23.0",
   args: [
     process.env.HEADLESS ? "--headless=chrome" : "",
     process.env.WSL ? "-no-sandbox" : "",
   ],
 };
 export default async function setup() {
-  const browser = await launch(puppeteer, DAPPETEER_DEFAULT_CONFIG);
+  // const browser = await launch(puppeteer, DAPPETEER_DEFAULT_CONFIG);
+  // const browser = await launch(puppeteer, DAPPETEER_DEFAULT_CONFIG);
+  // const { metaMask, browser } = await dappeteer.bootstrap();
+  const { browser, metaMask, metaMaskPage } = await bootstrap({
+    automation: "puppeteer",
+    browser: "chrome",
+    metaMaskVersion: "v10.14.0",
+    puppeteerOptions: {
+      args: [
+        process.env.HEADLESS ? "--headless=chrome" : "",
+        process.env.WSL ? "-no-sandbox" : "",
+      ],
+    },
+  });
+  // const { metaMask,browser } = await bootstrap();
   try {
     await createTransactionData.resetTestTmpFiles();
     await createTransactionData.createTransactionData(); // create test data
-    await setupMetamask(browser, {});
+    // await setupMetamask(browser, {});
     global.browser = browser;
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -26,7 +47,8 @@ export default async function setup() {
   process.env.PUPPETEER_WS_ENDPOINT = browser.wsEndpoint();
 
   global.browser = browser;
-  global.metamask = await getMetamaskWindow(browser);
+  // global.metamask = await getMetamaskWindow(browser);
+  global.metamask = metaMask;
 
   // await Config.getIns().initialize();
   await metamask.addNetwork({
