582b71f80121da9dd3f20bc041d8211c7084f473
Eason Gao <kaoimin@qq.com>
2023-03-01 11:57:30 +0800

    Revert "refactor(executor): change the get cell precompile contract  (#1047)"
    This reverts commit ce0f381375fe0580c5894ac62d41951a7da9c02e.

--- core/executor/src/precompiles/get_cell.rs
+++ core/executor/src/precompiles/get_cell.rs
@@ -1,15 +1,10 @@
-use ckb_types::{packed, prelude::*};
-use ethers::{
-    abi::{encode, parse_abi, FunctionExt, Token},
-    types::U256,
-};
 use evm::executor::stack::{PrecompileFailure, PrecompileOutput};
 use evm::{Context, ExitError, ExitSucceed};
 
-use protocol::types::{H160, H256};
+use protocol::types::H160;
 
 use crate::precompiles::{precompile_address, PrecompileContract};
-use crate::system_contract::image_cell::{CellInfo, CellKey};
+use crate::system_contract::image_cell::CellKey;
 use crate::{err, system_contract::image_cell::ImageCellContract};
 
 #[derive(Default, Clone)]
@@ -32,22 +27,14 @@ impl PrecompileContract for GetCell {
             }
         }
 
-        let (tx_hash, index) = parse_input(input)?;
-
-        let cell = ImageCellContract::default()
-            .get_cell(&CellKey { tx_hash, index })
+        let ret = ImageCellContract::default()
+            .get_cell(&CellKey::decode(input).map_err(|_| err!(_, "decode cell key"))?)
             .map_err(|_| err!(_, "get cell"))?;
-
-        let output = if let Some(cell) = cell {
-            encode_cell(cell)
-        } else {
-            encode_default()
-        };
 
         Ok((
             PrecompileOutput {
                 exit_status: ExitSucceed::Returned,
-                output,
+                output:      rlp::encode(&ret).to_vec(),
             },
             gas,
         ))
@@ -57,83 +44,4 @@ impl PrecompileContract for GetCell {
         let data_word_size = (input.len() + 31) / 32;
         (data_word_size * 3) as u64 + Self::MIN_GAS
     }
-}
-
-fn parse_input(input: &[u8]) -> Result<(H256, u32), PrecompileFailure> {
-    let contract = parse_abi(&[
-        "function getCell(bytes32 txHash, uint32 index) external returns (tuple(bool exists, bool hasTypeScript, bool hasConsumedNumber, uint64 createdNumber, uint64 consumedNumber, uint64 capacity, uint8 lockHashType, uint8 typeHashType, bytes lockCodeHash, bytes typeCodeHash, bytes lockArgs, bytes typeArgs, bytes data) memory)",
-    ]).map_err(|_| err!(_, "invalid abi"))?;
-
-    let function = contract
-        .functions()
-        .find(|fun| fun.selector() == input[0..4])
-        .ok_or(err!(_, "unknown selector"))?;
-
-    let func_input = function
-        .decode_input(&input[4..])
-        .map_err(|_| err!(_, "invalid function name"))?;
-
-    match &func_input[..] {
-        [Token::FixedBytes(tx_hash), Token::Uint(index)] => {
-            Ok((H256::from_slice(&tx_hash[..]), index.as_u32()))
-        }
-        _ => Err(err!(_, "invalid input")),
-    }
-}
-
-fn encode_cell(cell: CellInfo) -> Vec<u8> {
-    let cell_output = packed::CellOutput::new_unchecked(cell.cell_output);
-
-    let lock = cell_output.lock();
-    let lock_hash_type: u8 = lock.hash_type().into();
-
-    let type_ = cell_output.type_();
-    let (type_script_exists, type_hash_type, type_code_hash, type_args) =
-        if let Some(type_) = type_.to_opt() {
-            let hash_type: u8 = type_.hash_type().into();
-            (
-                true,
-                hash_type,
-                type_.code_hash().raw_data().to_vec(),
-                type_.args().raw_data().to_vec(),
-            )
-        } else {
-            (false, 0, Vec::with_capacity(32), Vec::new())
-        };
-
-    let capacity: u64 = cell_output.capacity().unpack();
-
-    encode(&[Token::Tuple(vec![
-        Token::Bool(true),
-        Token::Bool(type_script_exists),
-        Token::Bool(cell.consumed_number.is_some()),
-        Token::Uint(U256::from(cell.created_number)),
-        Token::Uint(U256::from(cell.consumed_number.unwrap_or(0))),
-        Token::Uint(U256::from(capacity)),
-        Token::Uint(U256::from(lock_hash_type)),
-        Token::Uint(U256::from(type_hash_type)),
-        Token::Bytes(lock.code_hash().as_bytes().to_vec()),
-        Token::Bytes(type_code_hash),
-        Token::Bytes(lock.args().as_bytes().to_vec()),
-        Token::Bytes(type_args),
-        Token::Bytes(cell.cell_data.to_vec()),
-    ])])
-}
-
-fn encode_default() -> Vec<u8> {
-    encode(&[Token::Tuple(vec![
-        Token::Bool(false),
-        Token::Bool(false),
-        Token::Bool(false),
-        Token::Uint(U256::from(0)),
-        Token::Uint(U256::from(0)),
-        Token::Uint(U256::from(0)),
-        Token::Uint(U256::from(0)),
-        Token::Uint(U256::from(0)),
-        Token::Bytes(Vec::new()),
-        Token::Bytes(Vec::new()),
-        Token::Bytes(Vec::new()),
-        Token::Bytes(Vec::new()),
-        Token::Bytes(Vec::new()),
-    ])])
 }
--- core/executor/src/precompiles/mod.rs
+++ core/executor/src/precompiles/mod.rs
@@ -11,7 +11,6 @@ mod ripemd160;
 mod rsa;
 mod secp256r1;
 mod sha256;
-
 #[cfg(test)]
 mod tests;
 mod verify_by_ckb_vm;
@@ -26,7 +25,7 @@ use protocol::types::H160;
 
 use crate::precompiles::{
     blake2_f::Blake2F, ec_add::EcAdd, ec_mul::EcMul, ec_pairing::EcPairing, ecrecover::EcRecover,
-    get_cell::GetCell, identity::Identity, modexp::ModExp, ripemd160::Ripemd160, sha256::Sha256,
+    identity::Identity, modexp::ModExp, ripemd160::Ripemd160, sha256::Sha256,
 };
 
 #[macro_export]
@@ -84,9 +83,7 @@ const fn precompile_address(addr: u8) -> H160 {
 }
 
 pub fn build_precompile_set() -> BTreeMap<H160, PrecompileFn> {
-    precompiles!(
-        EcRecover, Sha256, Ripemd160, Identity, ModExp, EcAdd, EcMul, EcPairing, Blake2F, GetCell
-    )
+    precompiles!(EcRecover, Sha256, Ripemd160, Identity, ModExp, EcAdd, EcMul, EcPairing, Blake2F)
 }
 
 pub(crate) fn read_point(input: &[u8], start: usize) -> Result<G1, PrecompileFailure> {
