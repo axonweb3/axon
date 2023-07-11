936a953f88fa60efbc0caad47fc3adddbe55233b
Eason <kaoimin@qq.com>
2023-07-11 15:11:02 +0800

    fix ci

--- protocol/src/codec/mod.rs
+++ protocol/src/codec/mod.rs
@@ -92,7 +92,7 @@ pub(crate) fn serialize_bytes<S>(val: &Bytes, s: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    faster_hex::withpfx_lowercase::serialize_str(val, s)
+    faster_hex::withpfx_lowercase::serialize(val, s)
 }
 
 pub(crate) fn serialize_uint<S, U>(val: &U, s: S) -> Result<S::Ok, S::Error>
