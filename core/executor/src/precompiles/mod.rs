e3cdf2f6fd6599c94a260608c677460f293a1d09
Felicity Yin <107106858+felicityin@users.noreply.github.com>
2023-08-07 15:55:02 +0800

    fix: struct CellOutput in solidiy example (#1308)

--- core/executor/src/precompiles/mod.rs
+++ core/executor/src/precompiles/mod.rs
@@ -28,8 +28,7 @@ use protocol::types::H160;
 
 use crate::precompiles::{
     blake2_f::Blake2F, call_ckb_vm::CallCkbVM, ec_add::EcAdd, ec_mul::EcMul, ec_pairing::EcPairing,
-    ecrecover::EcRecover, get_cell::GetCell, identity::Identity, modexp::ModExp,
-    ripemd160::Ripemd160, sha256::Sha256,
+    ecrecover::EcRecover, identity::Identity, modexp::ModExp, ripemd160::Ripemd160, sha256::Sha256,
 };
 
 #[macro_export]
@@ -95,8 +94,7 @@ const fn axon_precompile_address(addr: u8) -> H160 {
 
 pub fn build_precompile_set() -> BTreeMap<H160, PrecompileFn> {
     precompiles!(
-        EcRecover, Sha256, Ripemd160, Identity, ModExp, EcAdd, EcMul, EcPairing, Blake2F, GetCell,
-        CallCkbVM
+        EcRecover, Sha256, Ripemd160, Identity, ModExp, EcAdd, EcMul, EcPairing, Blake2F, CallCkbVM
     )
 }
 
