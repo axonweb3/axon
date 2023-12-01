bc1b91b94f99ab23125512c74340e21bf296a1ae
KaoImin <kaoimin@qq.com>
2023-12-01 18:31:37 +0800

    remove useless code

--- protocol/src/constants/configs.rs
+++ protocol/src/constants/configs.rs
@@ -15,7 +15,6 @@ pub const MAX_BLOCK_GAS_LIMIT: u64 = 30_000_000;
 // fee history request. Between 1 and 1024 blocks can be requested in a single
 // query. reference: https://docs.infura.io/infura/networks/ethereum/json-rpc-methods/eth_feehistory/
 pub const MAX_FEE_HISTORY: u64 = 1024;
-pub const MAX_RPC_GAS_CAP: u64 = 50_000_000;
 pub const BASE_FEE_PER_GAS: u64 = 0x539;
 
 const GWEI: u64 = 1_000_000_000;
