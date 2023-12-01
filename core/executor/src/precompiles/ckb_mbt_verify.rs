e6082f54706a18c63cde0ac2a569e4a78651f716
Eason Gao <kaoimin@qq.com>
2023-12-01 10:19:23 +0800

    feat: add ckb mbt proof verify precompile contract (#1578)
    * feat: add ckb MMR proof verify system contract
    
    * feat: add ckb mbt proof verify precompile
    
    * enable CMBTVerify
    
    * follow review opinion
    
    * cargo fmt

---
+++ core/executor/src/precompiles/ckb_mbt_verify.rs
@@ -0,0 +1,111 @@
+use ckb_types::packed;
+use ckb_types::prelude::Pack;
+use ckb_types::utilities::{merkle_root, MerkleProof};
+use ethers::abi::AbiDecode;
+use ethers::contract::{EthAbiCodec, EthAbiType};
+use evm::executor::stack::{PrecompileFailure, PrecompileOutput};
+use evm::{Context, ExitError, ExitSucceed};
+
+use protocol::types::H160;
+
+use crate::err;
+use crate::precompiles::{axon_precompile_address, PrecompileContract};
+
+#[derive(EthAbiCodec, EthAbiType, Clone, Debug, PartialEq, Eq)]
+pub struct VerifyProofPayload {
+    /// If the verify_type is 0, the leaves should be in the
+    /// raw_transactions_root, otherwise in the witnesses_root.
+    pub verify_type:           u8,
+    pub transactions_root:     [u8; 32],
+    pub witnesses_root:        [u8; 32],
+    pub raw_transactions_root: [u8; 32],
+    pub proof:                 Proof,
+}
+
+#[derive(EthAbiCodec, EthAbiType, Clone, Debug, PartialEq, Eq)]
+pub struct Proof {
+    pub indices: Vec<u32>,
+    pub lemmas:  Vec<[u8; 32]>,
+    pub leaves:  Vec<[u8; 32]>,
+}
+
+#[derive(Default, Clone)]
+pub struct CMBTVerify;
+
+impl PrecompileContract for CMBTVerify {
+    const ADDRESS: H160 = axon_precompile_address(0x07);
+    const MIN_GAS: u64 = 56000;
+
+    fn exec_fn(
+        input: &[u8],
+        gas_limit: Option<u64>,
+        _context: &Context,
+        _is_static: bool,
+    ) -> Result<(PrecompileOutput, u64), PrecompileFailure> {
+        let gas = Self::gas_cost(input);
+        if let Some(limit) = gas_limit {
+            if gas > limit {
+                return err!();
+            }
+        }
+
+        let payload = parse_input(input)?;
+        inner_verify_proof(payload)?;
+
+        Ok((
+            PrecompileOutput {
+                exit_status: ExitSucceed::Returned,
+                output:      vec![true.into()],
+            },
+            gas,
+        ))
+    }
+
+    fn gas_cost(_input: &[u8]) -> u64 {
+        Self::MIN_GAS
+    }
+}
+
+fn parse_input(input: &[u8]) -> Result<VerifyProofPayload, PrecompileFailure> {
+    <VerifyProofPayload as AbiDecode>::decode(input).map_err(|_| err!(_, "decode input"))
+}
+
+fn inner_verify_proof(payload: VerifyProofPayload) -> Result<(), PrecompileFailure> {
+    // Firstly, verify the transactions_root is consist of the raw_transactions_root
+    // and witnesses_root
+    let transactions_root: packed::Byte32 = payload.transactions_root.pack();
+    let raw_transactions_root: packed::Byte32 = payload.raw_transactions_root.pack();
+    let witnesses_root: packed::Byte32 = payload.witnesses_root.pack();
+
+    if merkle_root(&[raw_transactions_root.clone(), witnesses_root.clone()]) != transactions_root {
+        return Err(err!(_, "verify transactions_root fail"));
+    }
+
+    // Then, verify the given indices and lemmas can prove the leaves contains in
+    // the raw_transactions_root or the witnesses_root.
+    // If the verify_type is 0, the leaves should be in the raw_transactions_root,
+    // otherwise in the witnesses_root.
+    let lemmas = payload
+        .proof
+        .lemmas
+        .iter()
+        .map(|l| l.pack())
+        .collect::<Vec<_>>();
+    let leaves = payload
+        .proof
+        .leaves
+        .iter()
+        .map(|l| l.pack())
+        .collect::<Vec<_>>();
+    let verifying_root = if payload.verify_type == 0 {
+        raw_transactions_root
+    } else {
+        witnesses_root
+    };
+
+    if MerkleProof::new(payload.proof.indices, lemmas).verify(&verifying_root, &leaves) {
+        return Ok(());
+    }
+
+    Err(err!(_, "verify raw transactions root failed"))
+}
--- core/executor/src/precompiles/mod.rs
+++ core/executor/src/precompiles/mod.rs
@@ -1,6 +1,7 @@
 mod blake2_f;
 mod call_ckb_vm;
 mod ckb_blake2b;
+mod ckb_mbt_verify;
 mod ec_add;
 mod ec_mul;
 mod ec_pairing;
@@ -26,9 +27,9 @@ use evm::{Context, ExitError};
 use protocol::types::H160;
 
 use crate::precompiles::{
-    blake2_f::Blake2F, call_ckb_vm::CallCkbVM, ckb_blake2b::CkbBlake2b, ec_add::EcAdd,
-    ec_mul::EcMul, ec_pairing::EcPairing, ecrecover::EcRecover, identity::Identity, modexp::ModExp,
-    ripemd160::Ripemd160, sha256::Sha256,
+    blake2_f::Blake2F, call_ckb_vm::CallCkbVM, ckb_blake2b::CkbBlake2b, ckb_mbt_verify::CMBTVerify,
+    ec_add::EcAdd, ec_mul::EcMul, ec_pairing::EcPairing, ecrecover::EcRecover, identity::Identity,
+    modexp::ModExp, ripemd160::Ripemd160, sha256::Sha256,
 };
 
 #[macro_export]
@@ -95,7 +96,7 @@ const fn axon_precompile_address(addr: u8) -> H160 {
 pub fn build_precompile_set() -> BTreeMap<H160, PrecompileFn> {
     precompiles!(
         EcRecover, Sha256, Ripemd160, Identity, ModExp, EcAdd, EcMul, EcPairing, Blake2F,
-        CallCkbVM, CkbBlake2b
+        CallCkbVM, CkbBlake2b, CMBTVerify
     )
 }
 
