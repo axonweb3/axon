83abe129e66b51872c05d782be115bb974d63637
KaoImin <kaoimin@qq.com>
2023-12-13 10:32:52 +0800

    fix(mempool): check gas limit range

--- core/run/src/lib.rs
+++ core/run/src/lib.rs
@@ -345,7 +345,6 @@ where
         Arc::clone(storage),
         Arc::clone(trie_db),
         current_header.chain_id,
-        current_header.gas_limit.low_u64(),
         config.pool_size as usize,
         config.broadcast_txs_size,
         config.broadcast_txs_interval,
